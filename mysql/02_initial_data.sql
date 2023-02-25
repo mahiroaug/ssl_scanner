efbeb72638d7e33c5b765249a225578ec22aa3d7
mahiroaug <nrs020013@gmail.com>
2023-02-25 16:15:16 +0900

    check001

--- mysql/02_initial_data.sql
+++ mysql/02_initial_data.sql
@@ -1,10 +1,5 @@
 USE Certificates;
 
 INSERT INTO Certificates (Domain) VALUES
-('eonet.jp'),
-('support.eonet.jp'),
-('mineo.jp'),
-('optage.co.jp'),
-('api.mineo.jp'),
-('lalacall.jp'),
-('support.lalacall.jp');
+('github.com'),
+('slack.com'),
